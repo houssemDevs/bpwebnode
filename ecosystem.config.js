6f24b83ccef84a3286742b75a74f60b723a43113
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-02-21 13:25:37 +0100

    cleaning.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,15 @@ const path = require('path');
 module.exports = {
   apps: [
     {
-      name: 'worker',
-      script: path.resolve(__dirname, 'service.js'),
-      instances: 2,
-      exec_mode: 'cluster',
-      watch: true,
+      name: 'service',
+      script: path.resolve(__dirname, 'main.js'),
       env: {
         NODE_ENV: 'development',
         PORT: 3000,
       },
       env_production: {
         NODE_ENV: 'production',
+        PORT: 6000
       },
     },
   ],
