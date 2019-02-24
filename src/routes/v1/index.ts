02b927301eb5f7c6febcde51a99a484f82c6f3dd
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-02-24 14:53:29 +0100

    add versionning.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,12 +1,8 @@
 import koarouter from 'koa-router';
 
-import v1 from './v1';
-
 const router = new koarouter();
 
-
 // here use your routes.
-router.use('/v1', v1.routes());
-router.use('/v1', v1.allowedMethods());
+
 
 export default router;
