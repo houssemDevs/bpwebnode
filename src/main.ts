ffd457fe2a6c1c64dc8f23d0fe5188f107591d74
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-05 10:03:13 +0100

    add passport authentication

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import koa from 'koa';
 import koalogger from 'koa-logger-middleware';
+import koapassport from 'koa-passport';
 
 import router from './routes';
+import './services/passport';
 
 const server = new koa();
 
 server.use(koalogger());
+server.use(koapassport.initialize());
 
 server.use(router.routes());
 server.use(router.allowedMethods());
 
 const port = process.env.PORT || 4000;
 
-server.listen(port, () => console.log(`Plan controle service on ${port} ...`));
+server.listen(port, () => console.log(`my service on ${port} ...`));
---
+++ src/services/passport/index.ts
@@ -0,0 +1,7 @@
+import koapassport from 'koa-passport';
+
+// here add your strategies for authentication.
+//
+// Example:
+//  const LocalStrategy = new LocalStrategy();
+//  koapassport.use(LocalStrategy);
