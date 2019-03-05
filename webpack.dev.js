febc836fd977e434cef94505812cb9b8c32945ce
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-05 10:03:45 +0100

    setting the name of output to service.

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(require('./webpack.common'), {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
+    filename: 'service.js',
   },
   plugins: [new cleanBuildDir(['build'], { root: __dirname, verbose: true })],
 });
