c99a4c247a2a9f915bf8f177765150bdc62860ab
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-06 11:20:05 +0100

    get one value from sqlserver utiliy class.

---
+++ src/utils/sqlonevalue.ts
@@ -0,0 +1,38 @@
+import { ColumnValue, Connection, ConnectionConfig, Request } from 'tedious';
+
+export default class SqlOneValue {
+  private query: string;
+  private cnn: Promise<Connection>;
+  constructor(query: string, config: ConnectionConfig) {
+    this.query = query;
+    this.cnn = new Promise((res, rej) => {
+      const conn = new Connection(config);
+      conn.on('connect', err => {
+        if (err) {
+          rej(err);
+        } else {
+          res(conn);
+        }
+      });
+    });
+  }
+
+  public getValue(): Promise<ColumnValue[]> {
+    return new Promise((res, rej) => {
+      this.cnn.then(c => {
+        const req = new Request(this.query, (err, rowCount, rows) => {
+          if (err) {
+            rej(err);
+          } else if (rowCount <= 0) {
+            rej(new Error('no value found.'));
+          } else if (rowCount > 1) {
+            rej(new Error('multiple values.'));
+          } else {
+            res(rows);
+          }
+        });
+        c.execSql(req);
+      });
+    });
+  }
+}
