ff383bf9d1bf3b304d453d7f8a6e465d09e5b8f6
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-17 13:53:47 +0100

    server sent event middleware

---
+++ src/middlewares/sse.ts
@@ -0,0 +1,16 @@
+import { Context } from 'koa';
+import SimpleSSE from '../utils/sse';
+
+const sseMiddleware = async (ctx: Context, next) => {
+  if (ctx.headerSent) {
+    console.log(`Cannot set event stream headers. headers already sent.`);
+    await next();
+    return;
+  }
+  const sse = new SimpleSSE(ctx);
+  ctx.sse = sse;
+  ctx.body = sse;
+  await next();
+};
+
+export default sseMiddleware;
