1a857927a64e828b641c96db5f41169b14762757
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-18 11:41:07 +0100

    trying typedoc comments.

--- src/utils/sqlonevalue.ts
+++ src/utils/sqlonevalue.ts
@@ -1,14 +1,26 @@
-import { ColumnValue, Connection, ConnectionConfig, ConnectionOptions, Request } from 'tedious';
+import { ColumnValue, Connection, ConnectionConfig, Request } from 'tedious';
+
+// SqlOneValue will make an sql query fetch a single row of data then
+// return an object with column names as attributes.
 
 interface IResult {
-  [s:string]: any;
+  [s: string]: any;
 }
 
+/**
+ * @description make a single row sql query (return one row). Throw error if multiple rows.
+ */
 export default class SqlOneValue {
   private query: string;
   private cnn: Promise<Connection>;
+
+  /**
+   * Creates an instance of SqlOneValue.
+   * @param {string} query
+   * @param {ConnectionConfig} config
+   */
   constructor(query: string, config: ConnectionConfig) {
-    config.options = {...config.options, rowCollectionOnRequestCompletion: true };
+    config.options = { ...config.options, rowCollectionOnRequestCompletion: true };
     this.query = query;
     this.cnn = new Promise((res, rej) => {
       const conn = new Connection(config);
@@ -22,6 +34,11 @@ export default class SqlOneValue {
     });
   }
 
+  /**
+   * return a promise which will be resolved if any value is got from query,
+   * other wise the promise will be reject if no value is returned or multiple values are returnred.
+   * @returns {Promise<IResult>}
+   */
   public getValue(): Promise<IResult> {
     return new Promise((res, rej) => {
       this.cnn.then(c => {
@@ -33,7 +50,7 @@ export default class SqlOneValue {
           } else if (rowCount > 1) {
             rej(new Error('multiple values.'));
           } else {
-            res(rows.map(col => ({[col.metadata.colName]: col.value})).reduce((obj,v)=>({...obj, ...v}),{}));
+            res(rows.reduce((obj, v) => ({ ...obj, [v.metadata.colName]: v.value }), {}));
           }
         });
         c.execSql(req);
