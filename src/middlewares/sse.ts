be32d92ddb7b7304f376074356ef701e53dce6cb
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-18 10:09:26 +0100

    see middleware.

--- src/middlewares/sse.ts
+++ src/middlewares/sse.ts
@@ -1,22 +1,12 @@
 import { Context } from 'koa';
-import SimpleSSE from '../utils/sse';
+import SimpleSSE from '@utils/sse';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-const sseMiddleware = async (ctx: Context, next: any) => {
-=======
 export default (log: boolean = true, closeEvent: string = 'closeSSE') => async (
   ctx: Context,
   next: any,
 ) => {
->>>>>>> d41b141d6caa0c8d70d5e773d3f66d5d73b1e24c
   if (ctx.headerSent) {
-    console.log(`Cannot set event stream headers. headers already sent.`);
-=======
-export default async (ctx: Context, next: any) => {
-  if (ctx.headerSent) {
-    console.log(`[SSE]: header already sent, could not set SSE`);
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
+    console.log(`Cannot set server sent event headers. headers already sent.`);
     await next();
     return;
   }
@@ -25,11 +15,3 @@ export default async (ctx: Context, next: any) => {
   ctx.body = sse;
   await next();
 };
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-export default sseMiddleware;
-=======
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
-=======
->>>>>>> d41b141d6caa0c8d70d5e773d3f66d5d73b1e24c
