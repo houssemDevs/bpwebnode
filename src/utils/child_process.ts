6fd32ed11b0c3e8d371e9a1cc8e63456e9f67cc5
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-18 13:46:39 +0100

    add docs, kill worker when timeouts.

--- src/utils/child_process.ts
+++ src/utils/child_process.ts
@@ -5,19 +5,42 @@ interface IEnv {
   [s: string]: string;
 }
 
+/**
+ * Utiliy class help to fork nodejs worker, fire and forget style.
+ *
+ * @export
+ */
 export default class PromiseChildProcess {
   private script_file: string;
   private env: IEnv;
   private timeout: number;
+
+  /**
+   * Creates an instance of PromiseChildProcess.
+   * @param {string} file script file to run in the worker process.
+   * @param {IEnv} env envirnment variables to pass to the worker process.
+   * @param {number} [timeout=1000 * 60] total milliseconds to wait before rejecting the return value promise.
+   */
   constructor(file: string, env: IEnv, timeout: number = 1000 * 60) {
     this.script_file = file;
     this.env = env;
     this.timeout = timeout;
   }
+
+  /**
+   * fork the worker process and return a promise that resolve once the
+   *  child process return a value using process.send(val). if the timeouts reach
+   *  its limits and the worker does return nothing the promise is rejected.
+   *
+   * @returns {Promise<any>}
+   */
   public run(): Promise<any> {
     return new Promise((res, rej) => {
-      const t = setTimeout(() => rej(), this.timeout);
       const worker = cp.fork(this.script_file, [], { env: this.env });
+      const t = setTimeout(() => {
+        worker.kill();
+        rej(new Error('worker is taking too long.'));
+      }, this.timeout);
       worker.on('message', msg => {
         clearTimeout(t);
         res(msg);
