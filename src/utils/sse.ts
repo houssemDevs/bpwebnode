1d1c7c215fad197a53a493fb777d7fc945c3d434
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-18 10:13:01 +0100

    sse class.

--- src/utils/sse.ts
+++ src/utils/sse.ts
@@ -1,52 +1,4 @@
 import { Context } from 'koa';
-<<<<<<< HEAD
-import { Transform } from 'stream';
-import { isFunction, isObject, isString, isUndefined } from 'util';
-
-export default class SimpleSSE extends Transform {
-  private ctx: Context;
-  private closeEvent: string;
-  private log: boolean;
-  private ended: boolean;
-  private traceId: string;
-  constructor(
-    ctx: Context,
-    log: boolean = false,
-    traceId: string = '',
-    closeEvent: string = 'closeSSE',
-  ) {
-    super({ objectMode: true });
-    this.closeEvent = closeEvent;
-    this.log = log;
-    this.ctx = ctx;
-    this.ended = false;
-    this.traceId = traceId;
-    this.ctx.set({
-      'Cache-Control': 'no-cache',
-      "Connection": 'keep-alive',
-      'Content-Type': 'text/event-stream',
-    });
-    this.ctx.req.socket.setTimeout(0);
-    this.ctx.req.socket.setKeepAlive(true);
-    this.ctx.req.socket.setNoDelay(true);
-  }
-
-  public end(data: any): void;
-  // tslint:disable-next-line:unified-signatures
-  public end(data: any, encoding?: string): void;
-  public end(data: any, encoding?: string, callback?: () => void): void {
-    if (!data && !this.ended) {
-      data = { event: this.closeEvent };
-    }
-    this.ended = true;
-    super.end(data, encoding, callback);
-  }
-  public send(data: any, encoding?: string, callback?: () => void) {
-    if (arguments.length === 0 || this.ended) {
-      return false;
-    }
-    super.write(data, encoding, callback);
-=======
 import { Transform, TransformOptions } from 'stream';
 import { isObject } from 'util';
 
@@ -74,6 +26,9 @@ export default class SimpleSSE extends Transform {
     }
     super.write(data, encoding, done);
   }
+
+  public end(data: any): void;
+  public end(data: any, encoding?: string): void;
   public end(data: any, encoding?: string, done?: () => {}) {
     if (!data) {
       data = { event: this.closeEvent };
@@ -82,54 +37,23 @@ export default class SimpleSSE extends Transform {
     }
     this.ended = true;
     super.end(data, encoding, done);
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
   }
+  
   public _transform(data: any, encoding: string, done: () => void) {
     let subject: any;
     const resp = [];
     const prefix = 'data: ';
-<<<<<<< HEAD
-
-    if (isString(data)) {
-=======
     if (!isObject(data)) {
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
       subject = { data };
     } else {
       subject = data;
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
     if (subject.event) {
       resp.push(`event: ${subject.event}`);
     }
     if (subject.id) {
       resp.push(`id: ${subject.id}`);
     }
-<<<<<<< HEAD
-
-    if (isObject(subject.data)) {
-      resp.push(`${prefix}${JSON.stringify(subject.data)}`);
-    } else if (isUndefined(subject.data)) {
-      resp.push(`${prefix}`);
-    } else {
-      resp.push(`${prefix}${subject.data}`);
-    }
-    if (this.log) {
-      this._log(`Event sequence - ${resp.join(';')}`);
-    }
-    this.push(resp.join('\n').concat('\n\n'));
-    this.emit('message', resp.join('\n').concat('\n\n'));
-    if (this.ctx.body && isFunction(this.ctx.body.flush)) {
-      this.ctx.body.flush();
-    }
-    done();
-  }
-  private _log(msg: any) {
-    console.log(`[SimpleSSE:${this.traceId}]: ${msg}`);
-=======
     if (subject.relay) {
       resp.push(`relay: ${subject.relay}`);
     }
@@ -153,6 +77,5 @@ export default class SimpleSSE extends Transform {
       'Cache-Control': 'no-cache',
       "Connection": 'keep-alive',
     });
->>>>>>> 235faf419d44987fdd4d1e53a74e29c19613829f
   }
 }
