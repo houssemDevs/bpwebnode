c3dc0e41def0f0dd3fc9058efc81967db75a31cc
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-03-18 13:56:24 +0100

    ddd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   apps: [
     {
-      name: 'alerts_service',
+      name: 'service',
       script: path.resolve(__dirname, 'service.js'),
       env: {
         NODE_ENV: 'development',
